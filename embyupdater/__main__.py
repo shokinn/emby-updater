380fc77100e08affd302dc3a287453b9e884ce2a
Philip Henning <mail@philip-henning.com>
2019-01-30 20:49:52 +0100

    fix version display in binary and in pypi distribution

--- embyupdater/__main__.py
+++ embyupdater/__main__.py
@@ -1,4 +1,12 @@
-import embyupdater
+#!/usr/bin/env python3
+import os
+import sys
+
+try:
+    import embyupdater
+except ImportError:
+    sys.path.append(os.path.abspath('..'))
+    import embyupdater
 
 if __name__ == '__main__':
     embyupdater.main()
